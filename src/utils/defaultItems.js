6e8e8ebf75e76d51422a906ecb856e03bf7e4f84
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-05-31 20:42:06 +0100

    default items for task app

---
+++ src/utils/defaultItems.js
@@ -0,0 +1,92 @@
+
+export default function defaultItems() {
+  return [
+    {
+      title: "My Todos",
+      items: [
+        { text: "Welcome to my todo application", isCompleted: false },
+        { text: "This project was made by Taylor North", isCompleted: false },
+        {
+          text: "Using the React.js state management component framework",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos tempora ex earum ipsum suscipit vero repellendus labore rem quia!",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos tempora ex earum ipsum suscipit.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos tempora ex earum ipsum suscipit.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos tempora ex earum ipsum suscipit vero repellendus labore rem quia!",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius.",
+          isCompleted: false,
+        },
+        {
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga eius, dolores, dolorum quaerat optio dignissimos tempora ex earum ipsum suscipit.",
+          isCompleted: false,
+        },
+      ],
+    },
+    {
+      title: "Done",
+      items: [{ text: "Finish todo application", isCompleted: true }],
+    },
+    {
+      title: "Features",
+      items: [
+        {
+          text: "This app is able to create new lists to store new todos",
+          isCompleted: false,
+        },
+        {
+          text: "Vice versa you can delete lists as you please",
+          isCompleted: false,
+        },
+        {
+          text: "You can freely edit the todos created by clicking on the todo text",
+          isCompleted: false,
+        },
+        {
+          text: "List titles can be edited by clicking on them",
+          isCompleted: false,
+        },
+        { text: "You can mark todos completed", isCompleted: false },
+        { text: "You can delete todos", isCompleted: false },
+        {
+          text: "Todos are draggable to other list categories",
+          isCompleted: false,
+        },
+      ],
+    },
+    {
+      title: "Notes",
+      items: [
+        {
+          text: "If you have used Trello before you will notice alot of resemblance, the thought process of this was to remove the design phase and allow me to focus solely on the functionality of the application.",
+          isCompleted: false,
+        },
+      ],
+    },
+  ];
+}
