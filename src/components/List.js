2c048d9ab320693c0fc390c4d857184ae61dd671
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-01 13:22:11 +0100

    mouse down and mouse enter events

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function List({ children, clNa }) {
+function List({ children, clNa, onMseEnter, onMseDown, style }) {
   return (
-    <div className={clNa}>
+    <div className={clNa} onMouseEnter={(evt) => onMseEnter(evt)} style={style}>
+      <div className="drag-zone" onMouseDown={(evt) => onMseDown(evt)} />
       {children}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
