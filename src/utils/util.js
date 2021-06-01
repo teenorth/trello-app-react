2cb373ccac125ae8e7e116457aba49e2432c500c
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-01 13:24:35 +0100

    escape cases

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,10 +1,12 @@
 
 export function isItem(curIdx, thisIdx) {
+  if (!curIdx || !thisIdx) return false;
   if (thisIdx.listI === curIdx.listI && thisIdx.itemI === curIdx.itemI) return true;
   return false;
 }
 
 export function isList(curIdx, thisIdx) {
+  if (!curIdx || !thisIdx) return false;
   if (thisIdx.listI === curIdx.listI) return true;
   return false;
 }
