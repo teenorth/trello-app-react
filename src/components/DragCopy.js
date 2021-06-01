9887c3c161531c085b448171ee965107d56791bf
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-01 03:27:37 +0100

    fixed bug where mouse offset was forgotten

--- src/components/DragCopy.js
+++ src/components/DragCopy.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useMousePosition from "../utils/useMousePosition";
 import ReactDOM from "react-dom"
 
@@ -6,11 +6,15 @@ const portal = document.getElementById("portal");
 
 function DragCopy({ txt, copy }) {
   const { x, y } = useMousePosition();
+  const [mouse] = useState({
+    offsetY: copy.target.clientHeight / 2,
+    offsetX: copy.target.clientWidth / 2
+  })
 
   const getStyles = () => {
     return {
-      top: y ? y - copy.target.clientHeight / 2 : copy.clientY - copy.target.clientHeight / 2,
-      left: x ? x - copy.target.clientWidth / 2 : copy.clientX - copy.target.clientWidth / 2,
+      top: y ? y - mouse.offsetY : copy.clientY - mouse.offsetY,
+      left: x ? x - mouse.offsetX : copy.clientX - mouse.offsetX,
     }
   }
 
