137e63ffffdd96b5e19d44d749d49c47a03d2c30
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-01 15:54:47 +0100

    on drag event

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Item({ clNa, txt, mseDown, mseEnter }) {
+function Item({ clNa, txt, onDrag, mseEnter }) {
+
+  const handleDrag = (evt, txt) => {
+    evt.preventDefault();
+    onDrag(evt, txt)
+  }
+
   return (
-    <div
+    <div 
+      draggable
       className={clNa}
-      onMouseDown={(evt) => mseDown(evt, txt)}>
+      onDragStart={(evt) => handleDrag(evt, txt)}>
       <div className="drop-zone" onMouseEnter={() => mseEnter()} />
       {txt}
     </div>
