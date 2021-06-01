1f8b2ae5292b2f4a96195d9272a634b44de33f56
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-01 16:17:02 +0100

    item can now be edited and returns an event

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,66 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
-function Item({ clNa, txt, onDrag, mseEnter }) {
+function Item({ clNa, txt, onDrag, mseEnter, updateItem }) {
+  const [showEdit, setShowEdit] = useState(false);
 
   const handleDrag = (evt, txt) => {
     evt.preventDefault();
-    onDrag(evt, txt)
-  }
+    onDrag(evt, txt);
+  };
+
+  const handleUpdate = (data) => {
+    setShowEdit(false);
+    if (data === txt) return;
+    updateItem(data);
+  };
 
   return (
-    <div 
-      draggable
-      className={clNa}
-      onDragStart={(evt) => handleDrag(evt, txt)}>
-      <div className="drop-zone" onMouseEnter={() => mseEnter()} />
-      {txt}
+    <>
+      {!showEdit ? (
+        <div
+          draggable
+          className={clNa}
+          onDragStart={(evt) => handleDrag(evt, txt)}
+          onClick={() => setShowEdit(true)}>
+          <div className="drop-zone" onMouseEnter={() => mseEnter()} />
+          {txt}
+        </div>
+      ) : (
+        <EditItem current={txt} updateEvent={(data) => handleUpdate(data)} />
+      )}
+    </>
+  );
+}
+
+function EditItem({ current, updateEvent }) {
+  const [text, setText] = useState(current);
+  const selected = useRef(null);
+
+  const handleBlur = (event) => {
+    event.preventDefault();
+    selected.current = false;
+    if (!text) return setText(current);
+    updateEvent(text);
+  };
+
+  const textAreaCallback = (element) => {
+    if (element && !selected.current) {
+      element.select();
+      selected.current = true;
+    }
+  };
+
+  return (
+    <div className="item-edit">
+      <TextareaAutosize
+        ref={textAreaCallback}
+        onChange={(e) => setText(e.target.value)}
+        onBlur={(e) => handleBlur(e)}
+        value={text}
+        autoFocus
+        spellCheck="false"
+      />
     </div>
   );
 }
