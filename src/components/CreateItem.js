8497cb54c15b65ff5e5479890f18baf27d4e812b
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-03 13:53:42 +0100

    new items now scroll into view

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,45 +1,50 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import AddIcon from "./icons/AddIcon";
 import TextareaAutosize from "react-textarea-autosize";
 
 function CreateItem({ createItem }) {
   const [showCreate, setShowCreate] = useState(false);
   const [text, setText] = useState(null);
+  const ref = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
     if (!text) return setShowCreate(false);
     createItem(text);
     setText(null);
     setShowCreate(false);
+    ref.current.scrollIntoView();
   };
 
   return (
-    <>
+    <div className="create-item" onClick={() => setShowCreate(true)} ref={ref}>
       {!showCreate && (
-        <div className="create-item" onClick={() => setShowCreate(true)}>
+        <>
           <AddIcon size={14} onClick={() => null} />
           <span>Create new task</span>
-        </div>
+        </>
       )}
       {showCreate && (
         <form className="create-item-form">
           <TextareaAutosize
             placeholder="New task name"
-            onChange={(e) => setText(e.target.value)}
+            onChange={(evt) => setText(evt.target.value)}
             autoFocus
           />
           <div className="form-actions">
-            <div onClick={(evt) => handleSubmit(evt)}>
-              Add
-            </div>
-            <div onClick={() => setShowCreate(false)}>
+            <div onClick={(evt) => handleSubmit(evt)}>Add</div>
+            <div
+              onClick={(evt) => {
+                evt.stopPropagation();
+                setShowCreate(false);
+              }}>
               Close
             </div>
           </div>
         </form>
       )}
-    </>
+    </div>
   );
 }
 
